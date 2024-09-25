45bf02ad4a3e63d6d78bb45131b688eec8a33cc8
Tom Bridgwater <tom.bridgwater@zattoo.com>
2024-09-25 12:10:22 +0200

    SVG Icon generator

---
+++ public/icons/genpng.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+fg=${1:-black}
+bg=${2:-transparent}
+
+css="svg { stroke: $fg; fill: $fg; }"
+
+for s in 16 32 48 128; do
+  m=$(($s/16))
+  w=$(($s-$m-$m))
+  rsvg-convert icon.svg -o icon_${s}.png -w $w --page-width $s --page-height $s --top $m --left $m -b $bg --stylesheet <(echo "$css") 
+done
+
