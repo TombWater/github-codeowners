e1dee85b9468a89bdd313c4caa3a8ef5847a3438
Tom Bridgwater <tom.bridgwater@zattoo.com>
2024-09-26 17:29:52 +0200

    Extract github utils

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,20 +1,22 @@
 'use strict';
-import ignore from 'ignore';
 
 import './content.css';
-import {tokenStorage} from './storage';
+
+import { getPrInfo, getReviews, getOwnersMatchers } from './github';
 
 // For more information on Content Scripts,
 // See https://developer.chrome.com/extensions/content_scripts
 
 const decorateFileHeader = (node, folders) => {
   const path = node.dataset.path;
-  const match = folders.find(({folderMatch}) => folderMatch.ignores(path));
+  const match = folders.find(({ folderMatch }) => folderMatch.ignores(path));
   console.log('File Header', path, match.teams);
 
-  node.parentNode.querySelectorAll('.owners-decoration').forEach((decoration) => {
-    decoration.remove();
-  });
+  node.parentNode
+    .querySelectorAll('.owners-decoration')
+    .forEach((decoration) => {
+      decoration.remove();
+    });
 
   if (match) {
     const decoration = document.createElement('div');
@@ -29,107 +31,18 @@ const decorateFileHeader = (node, folders) => {
   }
 };
 
-
-const getPrInfo = () => {
-  const url = window.location.href;
-  const match = url.match(/github\.com\/([^/]+)\/([^/]+)\/pull\/(\d+)\/files/);
-  if (!match) {
-    return null;
-  }
-  return {
-    user: match[1],
-    repo: match[2],
-    num: match[3],
-   };
-};
-
 let cachedLastFileHeader;
 
 const getFileHeadersForDecoration = () => {
   const fileHeaders = document.querySelectorAll('div.file-header');
-  if (fileHeaders.length === 0  || cachedLastFileHeader === fileHeaders[fileHeaders.length - 1]) {
+  if (
+    fileHeaders.length === 0 ||
+    cachedLastFileHeader === fileHeaders[fileHeaders.length - 1]
+  ) {
     return [];
   }
   cachedLastFileHeader = fileHeaders[fileHeaders.length - 1];
   return fileHeaders;
-}
-
-const apiHeaders = async (pr) => {
-  const token = await tokenStorage.get();
-  console.log('Token', token);
-
-  const headers = {
-    Accept: 'application/vnd.github+json',
-    'X-GitHub-Api-Version': '2022-11-28',
-  };
-
-  if (token) {
-    headers.Authorization = `Bearer ${token}`;
-  }
-
-  return headers;
-};
-
-let cachedReviews = {};
-
-const getReviews = async (pr) => {
-  const url = `https://api.github.com/repos/${pr.user}/${pr.repo}/pulls/${pr.num}/reviews`;
-  const headers = await apiHeaders(pr);
-  const key = JSON.stringify({url, headers});
-
-  if (cachedReviews.key === key) {
-    return cachedReviews.reviews;
-  }
-
-  const response = await fetch(url, {headers});
-  const reviews = await response.json();
-
-  cachedReviews = {key, reviews};
-  return reviews;
-};
-
-let ownersCache = null;
-
-const getOwnersMatchers = async (pr) => {
-  if (ownersCache) {
-    return ownersCache;
-  }
-
-  const paths = [
-    '.github/CODEOWNERS',
-    'CODEOWNERS',
-    'docs/CODEOWNERS',
-  ];
-
-  const headers = await apiHeaders(pr);
-
-  for (const path of paths) {
-    const url = `https://api.github.com/repos/${pr.user}/${pr.repo}/contents/${path}`;
-    const response = await fetch(url, {headers});
-    const file = await response.json();
-    if (file.encoding === 'base64') {
-      const codeowners = atob(file.content);
-      const folders = [];
-      for (const line of codeowners.split('\n')) {
-          const trimmed = line.trim();
-
-          if (!trimmed.length || trimmed.startsWith('#')) {
-              continue;
-          }
-
-          const [folder, ...teams] = trimmed.split(/\s+/);
-
-          folders.push({
-              folderMatch: ignore().add(folder),
-              teams: new Set(teams),
-          });
-      }
-
-      ownersCache = folders.reverse();
-
-      return ownersCache;
-    }
-  }
 };
 
 // If we are on a PR files page, check which files have been approved
---
+++ src/github.js
@@ -0,0 +1,91 @@
+import ignore from 'ignore';
+
+import './content.css';
+import { tokenStorage } from './storage';
+
+export const getPrInfo = () => {
+  const url = window.location.href;
+  const match = url.match(/github\.com\/([^/]+)\/([^/]+)\/pull\/(\d+)\/files/);
+  if (!match) {
+    return null;
+  }
+  return {
+    user: match[1],
+    repo: match[2],
+    num: match[3],
+  };
+};
+
+const apiHeaders = async (pr) => {
+  const token = await tokenStorage.get();
+  console.log('Token', token);
+
+  const headers = {
+    Accept: 'application/vnd.github+json',
+    'X-GitHub-Api-Version': '2022-11-28',
+  };
+
+  if (token) {
+    headers.Authorization = `Bearer ${token}`;
+  }
+
+  return headers;
+};
+
+let cachedReviews = {};
+
+export const getReviews = async (pr) => {
+  const url = `https://api.github.com/repos/${pr.user}/${pr.repo}/pulls/${pr.num}/reviews`;
+  const headers = await apiHeaders(pr);
+  const key = JSON.stringify({ url, headers });
+
+  if (cachedReviews.key === key) {
+    return cachedReviews.reviews;
+  }
+
+  const response = await fetch(url, { headers });
+  const reviews = await response.json();
+
+  cachedReviews = { key, reviews };
+  return reviews;
+};
+
+let ownersCache = null;
+
+export const getOwnersMatchers = async (pr) => {
+  if (ownersCache) {
+    return ownersCache;
+  }
+
+  const paths = ['.github/CODEOWNERS', 'CODEOWNERS', 'docs/CODEOWNERS'];
+
+  const headers = await apiHeaders(pr);
+
+  for (const path of paths) {
+    const url = `https://api.github.com/repos/${pr.user}/${pr.repo}/contents/${path}`;
+    const response = await fetch(url, { headers });
+    const file = await response.json();
+    if (file.encoding === 'base64') {
+      const codeowners = atob(file.content);
+      const folders = [];
+      for (const line of codeowners.split('\n')) {
+        const trimmed = line.trim();
+
+        if (!trimmed.length || trimmed.startsWith('#')) {
+          continue;
+        }
+
+        const [folder, ...teams] = trimmed.split(/\s+/);
+
+        folders.push({
+          folderMatch: ignore().add(folder),
+          teams: new Set(teams),
+        });
+      }
+
+      ownersCache = folders.reverse();
+
+      return ownersCache;
+    }
+  }
+};
