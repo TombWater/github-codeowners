96fff207bfce5ba4897e7ae17ba1ecb2c0cbd40b
Tom Bridgwater <tom.bridgwater@zattoo.com>
2024-09-27 09:50:37 +0200

    Local instead of sync storage

--- src/storage.js
+++ src/storage.js
@@ -7,24 +7,25 @@
 // https://developer.chrome.com/extensions/declare_permissions
 
 export const tokenStorage = {
-    get: () => new Promise((resolve) => {
-        chrome.storage.sync.get(['token'], (result) => {
-          console.log('Token currently is', result.token);
-          resolve(result.token);
-        });
-      }),
-    set: (token) => new Promise((resolve) => {
-        chrome.storage.sync.set({token}, () => {
-            console.log('Token set to', token);
-            resolve(token);
-            }
-        );
+  get: () =>
+    new Promise((resolve) => {
+      chrome.storage.local.get(['token'], (result) => {
+        console.log('Token currently is', result.token);
+        resolve(result.token);
+      });
     }),
-    listen: (cb) => {
-        chrome.storage.onChanged.addListener((changes) => {
-            if (changes.token) {
-            cb(changes.token.newValue);
-            }
-        });
-    },
+  set: (token) =>
+    new Promise((resolve) => {
+      chrome.storage.local.set({token}, () => {
+        console.log('Token set to', token);
+        resolve(token);
+      });
+    }),
+  listen: (cb) => {
+    chrome.storage.onChanged.addListener((changes) => {
+      if (changes.token) {
+        cb(changes.token.newValue);
+      }
+    });
+  },
 };
