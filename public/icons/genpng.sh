9e034e2fb108ba744ec6aef04c702987797a68e8
Tom Bridgwater <tom.bridgwater@zattoo.com>
2024-10-09 17:32:11 +0200

    Use the great octocat pumpkin icon

--- public/icons/genpng.sh
+++ public/icons/genpng.sh
@@ -3,10 +3,10 @@
 fg=${1:-black}
 bg=${2:-transparent}
 
-css="svg { stroke: $fg; fill: $fg; }"
+css="svg { stroke: $fg; fill: $fg; } svg path { stroke: $fg; fill: $fg; }"
 
 for s in 16 32 48 128; do
-  m=$(($s/16))
+  m=0 # $(($s/16))
   w=$(($s-$m-$m))
   rsvg-convert icon.svg -o icon_${s}.png -w $w --page-width $s --page-height $s --top $m --left $m -b $bg --stylesheet <(echo "$css") 
 done
